28ea1aff3877c773760f6f168a5c31677f64eca6
Isabella Grizoni <isabella.grizoni@gmail.com>
2026-09-03 08:30:29 -0300

    Jogos adicionados

--- jogos.js
+++ jogos.js
@@ -696,7 +696,7 @@ const JOGOS = [
         fase: 'Copa do Brasil - Quartas de Final AGG 0X1'
       },
       {
-        id: 'j87',
+        id: 'j88',
         time1: 'Grêmio',
         time2: 'Internacional',
         hora: '20:00',
@@ -704,7 +704,7 @@ const JOGOS = [
         fase: 'Copa do Brasil - Quartas de Final AGG 0X0'
       },
       {
-        id: 'j88',
+        id: 'j89',
         time1: 'RB Bragantino',
         time2: 'Bahia',
         hora: '16:00',
@@ -712,7 +712,7 @@ const JOGOS = [
         fase: 'Brasileirão - Rodada 26'
       },
       {
-        id: 'j89',
+        id: 'j90',
         time1: 'São Paulo',
         time2: 'Atlético-MG',
         hora: '18:30',
@@ -720,7 +720,7 @@ const JOGOS = [
         fase: 'Brasileirão - Rodada 26'
       },
       {
-        id: 'j90',
+        id: 'j91',
         time1: 'Fluminense',
         time2: 'Vasco',
         hora: '21:00',
@@ -728,7 +728,7 @@ const JOGOS = [
         fase: 'Brasileirão - Rodada 26'
       },
       {
-        id: 'j91',
+        id: 'j92',
         time1: 'Coritiba',
         time2: 'Mirassol',
         hora: '11:00',
@@ -736,7 +736,7 @@ const JOGOS = [
         fase: 'Brasileirão - Rodada 26'
       },
       {
-        id: 'j92',
+        id: 'j93',
         time1: 'Cruzeiro',
         time2: 'Athletico-PR',
         hora: '16:00',
@@ -744,7 +744,7 @@ const JOGOS = [
         fase: 'Brasileirão - Rodada 26'
       },
       {
-        id: 'j93',
+        id: 'j94',
         time1: 'Internacional',
         time2: 'Santos',
         hora: '16:00',
@@ -752,7 +752,7 @@ const JOGOS = [
         fase: 'Brasileirão - Rodada 26'
       },
       {
-        id: 'j94',
+        id: 'j95',
         time1: 'Remo',
         time2: 'Flamengo',
         hora: '16:00',
@@ -760,7 +760,7 @@ const JOGOS = [
         fase: 'Brasileirão - Rodada 26'
       },
       {
-        id: 'j95',
+        id: 'j96',
         time1: 'Botafogo',
         time2: 'Palmeiras',
         hora: '18:30',
@@ -768,7 +768,7 @@ const JOGOS = [
         fase: 'Brasileirão - Rodada 26'
       },
       {
-        id: 'j96',
+        id: 'j97',
         time1: 'Corinthians',
         time2: 'Chapecoense',
         hora: '19:30',
@@ -776,7 +776,7 @@ const JOGOS = [
         fase: 'Brasileirão - Rodada 26'
       },
       {
-        id: 'j97',
+        id: 'j98',
         time1: 'Vitória',
         time2: 'Grêmio',
         hora: '20:00',
